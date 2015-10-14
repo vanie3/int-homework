d17f54a8f0dceafb86f95d154b7c3c168eea5df9
Vanessa Ceballos <Vanessa@decade-donald.sjvtech.com>
2015-10-14 09:26:32 -0700

    added app stylesheet and using gulp watch for it

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,29 +4,10 @@
         <title>Laravel</title>
 
         <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
-        <!-- Latest compiled and minified CSS -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
-
-        <style>
-            html, body {
-                height: 100%;
-            }
-
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato';
-            }
-
-            .title {
-                font-size: 96px;
-            }
-
-        </style>
+        <link rel="stylesheet" href="css/app.css"/>
     </head>
+
     <body>
         <div class="container">
             @yield('content')
@@ -34,5 +15,4 @@
     </body>
 
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
-
 </html>
